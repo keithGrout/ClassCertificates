6b64c5b13659473210cc5a626b2cb9ebba74825a
keithGrout <keiwi.g@gmail.com>
2013-08-06 15:04:50 -0700

    First commit

--- ClassCertificates/main.c
+++ ClassCertificates/main.c
@@ -6,13 +6,27 @@
 //  Copyright (c) 2013 Appology. All rights reserved.
 //
 
+#include <ctype.h>
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+void congratulateStudent(char *student, char *course, int numDays)
+{
+    printf("%s has done as much %s programming as i could fit into %d days. \n", student, course, numDays);
+}
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    congratulateStudent("Mark", "Cocoa", 5);
+    congratulateStudent("Bo", "Objective C", 2);
+    congratulateStudent("Mike", "Python", 5);
+    congratulateStudent("Ted", "iOS", 5);
+
+
     return 0;
 }
 
